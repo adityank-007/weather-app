180e45e72b500e5ac5f2ded38005ebc804b7e45a
Aditya <adityank.adi@gmail.com>
2021-11-23 23:50:41 -0500

    Add react-ui

--- weather-ui/src/App.js
+++ weather-ui/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'; 
+
 
 function App() {
+  const [ displayData, setDisplayData ] = useState('');
+  const [ location, setLocation ]   = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Weather App</h1>
+      <h2> Please Enter the Location</h2>
+      <form onSubmit={(event) => handleSubmit(event, setDisplayData, location)}> 
+        <input type="text" 
+        placeholder="Enter your The Location Here"
+        onChange= { (event) => handleChange(event,setLocation,) }  />
+        <button>Submit</button>
+      </form>
+      
+    {  displayData && <h2> The Current Temperature is {displayData} F</h2>}
     </div>
+    
   );
 }
+function handleChange(event, setDisplayData) {setDisplayData(event.target.value);}
+
+function handleSubmit(event, setDisplayData, location) {
+  event.preventDefault();
+  if (location && location.length > 0) {
+    getWeather(location,setDisplayData);
+  }
+}
+function handleErrors(response) {
+  if (!response.ok) {
+      alert("Please Enter a valid Location");
+  }
+  return response;
+}
+function getWeather(location, setDisplayData) {
+
+  fetch('http://localhost:8080/v1/weather-data/' + location)
+  .then(handleErrors)
+  .then(response => { console.log(response); return response.json() })
+  .then(data => { 
+    console.log(data);
+    var currentTeperature = data.currentTemperature;
+  
+  setDisplayData(currentTeperature);}).catch(err => console.log(err));
+}
+
 
 export default App;
